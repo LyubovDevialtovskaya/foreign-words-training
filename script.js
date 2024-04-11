c318bd13b1bf4b8ddf30fec79d5565c22abe9f88
Lyubov Devialtovskaya <84678150+LyubovDevialtovskaya@users.noreply.github.com>
2024-04-11 16:03:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const cardFront = document.querySelector('#card-front');
 const frontTitle = cardFront.querySelector('h1');
 const cardBack = document.querySelector('#card-back');
 const backTitle = cardBack.querySelector('h1');
-const example = cardFront.querySelector('span'); // Changed to cardFront
+const example = cardFront.querySelector('span'); 
 const back = document.querySelector('#back');
 const testing = document.querySelector('#exam');
 const next = document.querySelector('#next');
@@ -47,7 +47,7 @@ function prepareCard(word) {
     currentWord.textContent = currentIndex + 1;
     frontTitle.textContent = word.title;
     backTitle.textContent = word.translation;
-    example.textContent = word.example; // Display example on the front side
+    example.textContent = word.example; 
     wordsProgress.value = (currentIndex + 1) / arr.length * 100;
 }
 
