187c9f5335cb0a2c13e3de1a3e4844ebb040aad6
Lyubov Devialtovskaya <84678150+LyubovDevialtovskaya@users.noreply.github.com>
2024-04-15 12:34:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,9 @@ const backTitle = cardBack.querySelector('h1');
 const example = cardBack.querySelector('span');
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
-const currentWord = document.querySelector('#current-word');
-const totalWord = document.querySelector('#total-word');
-const wordsProgress = document.querySelector('#words-progress');
-const shuffleWords = document.querySelector('#shuffle-words');
-const testing = document.querySelector('#testing');
-const flipCard = document.querySelector('.flip-card');
-const cardFront = document.querySelector('#card-front');
-const frontTitle = cardFront.querySelector('h1');
-const cardBack = document.querySelector('#card-back');
-const backTitle = cardBack.querySelector('h1');
-const example = cardBack.querySelector('span');
-const back = document.querySelector('#back');
-const next = document.querySelector('#next');
 const studyCards = document.querySelector('.study-cards');
 const examCards = document.querySelector('#exam-cards');
+const examination = document.querySelector('#examination');
 
 function randomInteger(max) {
     return Math.floor(Math.random() * (max + 1));
@@ -89,62 +77,12 @@ shuffleWords.addEventListener('click', function() {
 totalWord.textContent = arr.length;
 
 testing.addEventListener('click', function() {
-    constructor(title, translation, example) {
-        this.title = title;
-        this.translation = translation;
-        this.example = example;
-    }
-}
-
-const word1 = new Word("сок", "juice", "I like orange juice.");
-const word2 = new Word("солнце", "sun", "The sun gives you a good mood when it shines.");
-const word3 = new Word("жизнь", "life", "Life is beautiful, the main thing is to notice it");
-const word4 = new Word("платье", "dress", "A dress is the best decoration for a girl.");
-const word5 = new Word("путешествие", "journey", "Journey of a lifetime!");
-
-const arr = [word1, word2, word3, word4, word5];
-
-flipCard.addEventListener("click", function() {
-    this.classList.toggle('active');
+    studyCards.classList.add('hidden');
+    examCards.classList.remove('hidden');
+    addTestCards();
 });
 
-let currentIndex = 0;
-
-function prepareCard(word) {
-    currentWord.textContent = currentIndex + 1;
-    frontTitle.textContent = word.title;
-    backTitle.textContent = word.translation;
-    example.textContent = word.example;
-    wordsProgress.value = (currentIndex + 1) / arr.length * 100;
-}
-
-prepareCard(arr[currentIndex]);
-
-next.addEventListener("click", function() {
-    currentIndex++;
-    prepareCard(arr[currentIndex]);
-    back.disabled = false;
-    if (currentIndex === arr.length - 1) {
-        next.disabled = true;
-    }
-});
-
-back.addEventListener("click", function() {
-    currentIndex--;
-    prepareCard(arr[currentIndex]);
-    next.disabled = false;
-    if (currentIndex === 0) {
-        back.disabled = true;
-    }
-});
-
-shuffleWords.addEventListener('click', function() {
-    arr.sort(() => Math.random() - 0.5);
-    prepareCard(arr[currentIndex]);
-});
-
-totalWord.textContent = arr.length;
-
+// функция тестирования карточек
 testing.addEventListener('click', function() {
     studyCards.classList.add('hidden');
     examCards.classList.remove('hidden');
@@ -210,7 +148,7 @@ function checkTranslationsHandler(currentCard) {
         } else {selectedCard.classList.add('correct');
         currentCard.classList.add('wrong');
         setTimeout(() => {
-            const cards = document.querySelectorAll('.card');
+            const cards = document.querySelectorAll('card');
             cards.forEach(card => {
                 card.classList.remove('correct');
                 card.classList.remove('wrong');
